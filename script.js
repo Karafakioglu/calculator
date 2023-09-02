03a73e26b55629ae4e237c643c2615ed8cd79c37
Karafakioglu <ismailkarafakioglu@gmail.com>
2023-09-02 23:24:34 +0200

    added the operate funcs

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const calculatorBody = document.querySelector(".calculator-body");
 const calculatorButtons = calculatorBody.querySelectorAll("button");
+const operatorButtons = document.querySelectorAll(".operator-btn");
+const calculateButton = document.getElementById("calculate-btn");
+const display = document.getElementById("display");
+
 let displayValue = "";
 
-// document.getElementById("delete-btn").addEventListener("click", () =>{
-//     console.log(displayValue);
-// })
+calculateButton.addEventListener("click", ()=>{
+    operate(parseUserInput()[0],parseUserInput()[1],parseUserInput()[2])
+    // console.log(displayValue)
+    console.log(parseUserInput())
+    display.innerText = operate(parseUserInput()[0],parseUserInput()[1],parseUserInput()[2]);
+    displayValue = "";
 
+});
+
+
+
+document.getElementById("delete-btn").addEventListener("click", () =>{
+    console.log(displayValue);
+})
+
+
+function parseUserInput(){
+    let splitUserInput = displayValue.split(/([\+\-\*\/])/, 3);
+    return splitUserInput;
+}
 
 function displayNumbers(){
     calculatorButtons.forEach(button =>{
@@ -13,10 +33,19 @@ function displayNumbers(){
             if(button.className === "number-btn"){
                 
                 displayValue = displayValue + button.innerText; 
-                document.getElementById("display").innerText = displayValue
+                display.innerText = displayValue
             }
         })
     })
+
+    operatorButtons.forEach(operatorBtn =>{
+        operatorBtn.addEventListener("click", () =>{
+            displayValue = displayValue + operatorBtn.innerText;
+            display.innerText = displayValue
+
+        })
+    })
+    
 }
 
 function clearDisplay(){
@@ -28,20 +57,27 @@ function clearDisplay(){
     })
 }
 
-function operate(operator, firstNum, secondNum){
+function operate(firstNum, operator, secondNum){
+    firstNum = parseInt(firstNum);
+    secondNum = parseInt(secondNum);
     if(operator === "+"){
+        console.log(firstNum+secondNum);
         return firstNum+secondNum
     }
     else if(operator === "-"){
+        console.log(firstNum-secondNum);
         return firstNum-secondNum;
     }
     else if(operator === "*"){
+        console.log(firstNum*secondNum);
         return firstNum*secondNum;
     }
     else if(operator === "/"){
+        console.log(firstNum/secondNum);
         return firstNum/secondNum;
     }
 }
+
 
 
 function main(){
